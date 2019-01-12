1bd1f5968389f5da56bef6b4ee81c8c628d4d597
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-12 19:22:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,8 @@ contacts.each do |person, data|
 data.each do |attribute, value|
    
   if attribute == :favorite_icecream_flavors
-      value.delete_if {|flavor| == flavor = "strawberry" 
+      value.delete_if {|flavor| flavor == "strawberry"}
+      end
       end
     end
   #remember to return your newly altered contacts hash!
